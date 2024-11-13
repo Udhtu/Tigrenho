e225307d73f8d5b9182e6ea60a901d2420944180
Udhtu_Max <joaodiel44@gmail.com>
2024-11-13 00:03:02 -0300

    Modificação e injetação de código para funcionamento da roleta de classes

--- função.js
+++ função.js
@@ -15,7 +15,7 @@ function determinarClasse(numeroAleatorio) {
         23: 'Perdeu Muito',
         55: 'Perdeu Pouco',
         73: 'Ganhou Pouco',
-        101: 'Ganhou Muito'
+        101: 'Ganhemo demais'
     };
 
     if (numeroAleatorio <= chancePerder) {
@@ -58,6 +58,29 @@ function gerarFuncaoAleatoria() {
     } else {
         console.error('Elemento com ID "resultado" não encontrado.');
     }
+    const imgURL = imgclasse(classe);
+    const imagemElement = document.getElementById("imagem");
+
+    if (imagemElement) {
+        imagemElement.src = imgURL;
+    } else {
+        console.error('Elemento com ID "imagem" não encontrado.');
+    }
+}
+ 
+function imgclasse(classe) {
+// Cria um objeto para mapear valores a URLs de imagem
+    const imageMap = {
+       "Ganhemo demais": "https://cdn.discordapp.com/attachments/800172094783881226/1306082508831592529/sack.png?ex=67355f9f&is=67340e1f&hm=c296198af656727eb029750f22dc5ba6dac590810639217295d7a79171b43363&z",
+       "Ganhou Pouco": "https://cdn.discordapp.com/attachments/800172094783881226/1306082506466000997/5yen.png?ex=67355f9f&is=67340e1f&hm=399e6e4e431cec70bf469f7e56862d1b61c2232b429d2d9e4b7727f66036b25f&",
+       "Perdeu Muito": "https://cdn.discordapp.com/attachments/800172094783881226/1306082508600774656/merda.png?ex=67355f9f&is=67340e1f&hm=ad773a03d3d96c73dc855ae5f41827f83065ec7da30b0a8ccf5d63bb9c3ff3d4&",
+       "Perdeu Pouco": "https://cdn.discordapp.com/attachments/800172094783881226/1306082508600774656/merda.png?ex=67355f9f&is=67340e1f&hm=ad773a03d3d96c73dc855ae5f41827f83065ec7da30b0a8ccf5d63bb9c3ff3d4&",
+       "Perdeu Tudo": "https://cdn.discordapp.com/attachments/800172094783881226/1306082507900452914/maca.png?ex=67355f9f&is=67340e1f&hm=1df799eb98ce6488d2b12fd86f22b4764d317bc7cfe4aaa43153deb592e7491c&",
+       "Duplica": "https://cdn.discordapp.com/attachments/800172094783881226/1306082507380359238/diamante.png?ex=67355f9f&is=67340e1f&hm=346ddcccca2ca5548a2c00cc864feaa7138d71c5924eb5021ecdc4fd887c54af&"
+       // Adicione mais valores conforme necessário
+    };
+
+    return imageMap[classe] || "https://exemplo.com/imagem_padrao.jpg";
 }
 
 document.getElementById("gerar").addEventListener("click", gerarFuncaoAleatoria);
